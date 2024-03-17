ee9e82d91c0975b5362b11b4abed5a0496ae279d
BurhanArslann <arslann.burhan.git@gmail.com>
2024-03-17 20:40:49 +0300

    hafta10 final

--- hafta10/src/App.js
+++ hafta10/src/App.js
@@ -1,8 +1,16 @@
-import logo from './logo.svg';
-
+import { useState } from "react";
+import UlkeListe from "./UlkeListe";
 
 function App() {
- 
+return(
+  <>
+  <div className="container my-3">
+  <UlkeListe/>
+  </div>
+  </>
+)
+
+
 }
 
 export default App;
